5a0580c689d31e99348f780a6fb733cb9bff48ef
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-10-06 22:31:58 -0400

    Successfully reorganized game board

--- src/js/board.js
+++ src/js/board.js
@@ -1,22 +1,24 @@
 import $ from 'jquery';
 function Board(){
-	this.columns = [];
-	 for(let y=0; y<7; y++){
-	 	let column = [];
-		for (let x=0; x<6; x++) {
-			let rowSpace = [];
-			column.push(rowSpace);
+	this.rows = [];
+	for(let y=0; y<6; y++){
+	 	let row = [];
+		for (let x=0; x<7; x++) {
+			let column = []
+			row.push(column);
 		}
-		this.columns.push(column);
-	 }
-
-	this.columns.forEach(function(column, index){
-		let columnIndex = index;
-		let boardColumnIndex = ".board-column-"+columnIndex;
-		$("#board").append(`<div class="board-column-${index} board-column"></div>`)
-		column.forEach(function(rowSpace, index, columnIndex){
-			$(boardColumnIndex).append(`<div class="row-space-${index} row-space"></div>`)
+		this.rows.push(row);
+	}
+	let spaceCounter = 0;
+	this.rows.forEach(function(row, index){
+		let rowIndex = index;
+		let boardRowIndex = ".board-row-"+rowIndex;
+		$("#board").append(`<div class="board-row-${index} board-row"></div>`)
+		row.forEach(function(column, index){
+			$(boardRowIndex).append(`<span data-column=${index} data-row=${rowIndex} data-space=${spaceCounter} data-filled="false" class="board-column space"></span>`);
+			spaceCounter++;
 		})
 	})
+	console.log(this.rows);
 }
 export {Board};
